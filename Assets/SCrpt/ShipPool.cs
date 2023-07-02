7efe39ebe4049954bb5015f0739264b8cea5490d
Wariouss <nvr.vbobrakov@gmail.com>
2023-07-02 18:57:29 +0100

    Fixed
    Changed class name

--- Assets/SCrpt/ShipPool.cs
+++ Assets/SCrpt/ShipPool.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class S : MonoBehaviour
+public class ShipPool : MonoBehaviour
 {
     public SpikeScript shipPrefab;
     public int size;
